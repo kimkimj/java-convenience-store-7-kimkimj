87d6544b3783cec6c828bba740f18d58727659ce
u <mjkim2304@gmail.com>
2024-11-11 22:37:30 +0900

    feat: composes the application

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,20 +1,33 @@
 package store;
 
 import camp.nextstep.edu.missionutils.Console;
+import store.domain.Order;
 import store.domain.Stock;
+import store.service.OrderService;
 import store.view.FileReader;
 import store.view.InputView;
 import store.view.OutputView;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 public class Application {
     public static void main(String[] args)  {
         FileReader fileReader = new FileReader();
-        Map<String, Stock> map = fileReader.readFile();
-        OutputView outputView = new OutputView(map);
-        outputView.printProducts();
-        //Console.close();
+        InputView inputView = new InputView();
+        Map<String, Stock> stockMap = fileReader.readFile();
+        OrderService orderService = new OrderService(stockMap);
+        boolean morePurchase = true;
+
+        while (morePurchase) {
+            OutputView outputView = new OutputView(stockMap);
+            outputView.printProducts();
+
+            List<Order> orders = inputView.readItem();;
+            orderService.order(orders);
+
+            morePurchase = inputView.promptForMorePurchase();
+        }
     }
 }
